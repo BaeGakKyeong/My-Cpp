17686b696f52993ff0ca2a22cc26ecc10cb1ab7c
BaeGakKyeong <bggcarl@gmail.com>
2025-03-27 10:59:47 +0900

    test for compile

--- output.cpp
+++ output.cpp
@@ -4,5 +4,7 @@ int main(){
     int n = 3;
     char c = '#';
 
-    std::cout << c << 5.5 << '- ' << n << "hello" << true;
+    std::cout << c << 5.5 << '-' << n << "hello" << true;
+
+    return 0;
 }
