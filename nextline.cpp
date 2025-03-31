eb22951dca5fc084c1031091579a3a5e893de8e5
BaeGakKyeong <bggcarl@gmail.com>
2025-03-31 15:02:09 +0900

    print string with stream insertion operator

---
+++ nextline.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    std::cout << "hello world \n";  //hello world를 출력하고 다름 줄로 넘어감
+    std::cout << "첫 번째 맛보기 입니다.\n\n\n";
+
+    std::cout << "hello? \n" << "두 번째 맛보기 입니다.";
+
+    return 0;
+}
