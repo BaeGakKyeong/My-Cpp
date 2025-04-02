70b6af92f3dfc80468120a81474d4e6e26c01229
BaeGakKyeong <bggcarl@gmail.com>
2025-04-02 16:41:13 +0900

    add a coment

--- input.cpp
+++ input.cpp
@@ -10,7 +10,7 @@ int main(){
     cout << "높이를 입력하시오 >> ";
 
     int hight;
-    cin >> hight;
+    cin >> hight;       //cin >> width >> hight; 도 가능
 
     int area = hight * width;
     cout << "면적은 " << area << endl;
